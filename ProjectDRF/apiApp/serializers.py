9b0c44b1c43e9d1584bc3d7611a860270bde5339
Saumya <saumya.mishra@intelliatech.com>
2024-08-06 19:28:48 +0530

    session added

---
+++ ProjectDRF/apiApp/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import  *
+from django.contrib.auth.models import User
+
+
+ 
+    
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['cat_name']
+
+class BookSerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    class Meta:
+        model = Book
+        fields = '__all__'
+    
+    def create(self, validated_data):
+        category_data = validated_data.pop('category')
+        category, created = Category.objects.get_or_create(**category_data)
+        book = Book.objects.create(category=category, **validated_data)
+        return book
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username','email','password']
+
+    def create(self, validated_data):
+        # Extract password and create user with password hashing
+        user = User.objects.create_user(username=validated_data['username'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
